7f9f053fc45b5e184d8793b5d56a46c81baa9680
irtaza safi <irtaza safi>
2016-01-08 04:18:11 +0500

    current location working

--- app/src/main/java/com/example/irtazasafi/mnemorizer/signup.java
+++ app/src/main/java/com/example/irtazasafi/mnemorizer/signup.java
@@ -1,13 +1,83 @@
 package com.example.irtazasafi.mnemorizer;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class signup extends AppCompatActivity {
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class signup extends AppCompatActivity  {
+
+    public String serverResponse = "";
+    public String serverURL = "http://192.168.10.7";
+    public volatile boolean respRecieved = false;
+
+    LocationManager locationManager;
+
+    public final static boolean isValidEmail(CharSequence target) {
+
+        return target != null && android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches();
+    }
+
+    public static String HashPassword(String plaintext) throws NoSuchAlgorithmException {
+        MessageDigest m = MessageDigest.getInstance("MD5");
+        m.reset();
+        m.update(plaintext.getBytes());
+        byte[] digest = m.digest();
+        BigInteger bigInt = new BigInteger(1, digest);
+        String hashtext = bigInt.toString(16);
+        return hashtext;
+    }
+
+    public void makeSynchronusRequest(final String url, final String method) throws Exception {
+        Thread requester = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    HttpURLConnection koka = (HttpURLConnection) new URL(url).openConnection();
+                    koka.setRequestMethod(method);
+                    InputStream response = koka.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(response));
+                    for (String line; (line = reader.readLine()) != null; ) {
+                        serverResponse = serverResponse + line;
+                    }
+                    System.out.println(serverResponse);
+                    respRecieved = true;
+                } catch (Exception e) {
+                }
+            }
+
+        });
+        requester.start();
+    }
+
+    public void showToast(String text) {
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +87,38 @@ public class signup extends AppCompatActivity {
         setSupportActionBar(toolbar);
     }
 
-    public void signUpAndLogin(View view) {
-        
+    @TargetApi(Build.VERSION_CODES.M)
+    public void signUpAndLogin(View view) throws Exception {
+        EditText email = (EditText) findViewById(R.id.emailInSignUp);
+        EditText password = (EditText) findViewById(R.id.passwordInSignUp);
+        EditText passwordconfirm = (EditText) findViewById(R.id.confirmPasswordSignUp);
+
+
+
+
+        GPSTracker gps = new GPSTracker(this);
+        if(!gps.canGetLocation()) {
+            showToast("PLEASE ENABLE GPS AND TRY AGAIN");
+            gps.showSettingsAlert();
+            return;
+        }
+        double latitude = gps.getLatitude();
+        double longitude = gps.getLongitude();
+
+
+        String query = serverURL+"/"+"-signUpRequest-"+email.getText().toString()+"-"+HashPassword(password.getText().toString())+
+                "-"+String.valueOf(latitude)+"-"+String.valueOf(longitude);
+
+        serverResponse ="";
+        respRecieved= false;
+        makeSynchronusRequest(query,"GET");
+        while(!respRecieved){
+            System.out.println("Waiting for Response");
+        }
+        //String resp[] = serverResponse.split("-");
+        System.out.println("Response is " + serverResponse);
+
+
 
     }
-
 }
